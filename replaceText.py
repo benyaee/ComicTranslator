efaea040d2a7cd5d4aa22897a2219e7faf8ed71b
benyaee <59710403+benyaee@users.noreply.github.com>
2021-12-17 14:31:51 -0500

    Update replaceText.py

--- replaceText.py
+++ replaceText.py
@@ -6,7 +6,7 @@ from PIL import Image, ImageDraw, ImageFont
 import inpaint
 import translate
 
-os.environ["GOOGLE_APPLICATION_CREDENTIALS"]="C:\\Users\\benre\\OneDrive\\Desktop\\ComicTranslator\\total-cedar-335014-5412f0edd455.json"
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"]="Put your .json Google Cloud API Path Key Here"
 client = vision.ImageAnnotatorClient()
 path = "msedge_fiCagpYbjE.png"
 def fileName(file):
